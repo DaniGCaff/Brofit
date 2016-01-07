a7cca0705efc2c0f6b6ba7e7f3dc13abd10ae4db
Rober <danigcaff@gmail.com>
2016-01-07 18:17:52 +0100

    7-1-16

---
+++ src/model/DatosObjetivo.java
@@ -0,0 +1,46 @@
+package model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+/**
+ * The persistent class for the objetivos_has_ejercicios database table.
+ * 
+ */
+@Entity
+@Table(name="DatosObjetivo")
+@NamedQuery(name="DatosObjetivo.findAll", query="SELECT d FROM DatosObjetivo d")
+public class DatosObjetivo implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@EmbeddedId
+	private DatosObjetivoPK id;
+
+	@Column(name="numeroEjercicios")
+	private float numeroEjercicios;
+	
+	@Column(name="numeroSeries")
+	private float numeroSeries;
+
+	//bi-directional many-to-one association to Objetivo
+	@ManyToOne
+	@JoinColumn(name="Objetivos_idObjetivos", nullable=false, insertable=false, updatable=false)
+	private Objetivo objetivo;
+	
+	@Column(name="tamanoMuscular", nullable=false, insertable=false, updatable=false)
+	private int tamanoMuscular;
+
+	@Column(name="tipoEjercicio", nullable=false, insertable=false, updatable=false)
+	private int tipoEjercicio;
+
+	public DatosObjetivo() {
+	}
+
+}
---
+++ src/model/DatosObjetivoPK.java
@@ -0,0 +1,48 @@
+package model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class DatosObjetivoPK implements Serializable {
+	//default serial version id, required for serializable classes.
+	private static final long serialVersionUID = 1L;
+
+	@Column(unique=true, nullable=false)
+	private int tamanoMuscular;
+	
+	@Column(unique=true, nullable=false)
+	private int tipoEjercicio;
+
+	@Column(unique=true, nullable=false)
+	private int Objetivos_idObjetivos;
+
+	public DatosObjetivoPK() {
+	}
+
+	public boolean equals(Object other) {
+		if (this == other) {
+			return true;
+		}
+		if (!(other instanceof DatosObjetivoPK)) {
+			return false;
+		}
+		DatosObjetivoPK castOther = (DatosObjetivoPK)other;
+		return 
+			(this.tamanoMuscular == castOther.tamanoMuscular)
+			&& (this.tipoEjercicio == castOther.tipoEjercicio)
+			&& (this.Objetivos_idObjetivos == castOther.Objetivos_idObjetivos);
+	}
+
+	public int hashCode() {
+		final int prime = 31;
+		int hash = 17;
+		hash = hash * prime + this.tamanoMuscular;
+		hash = hash * prime + this.tipoEjercicio;
+		hash = hash * prime + this.Objetivos_idObjetivos;
+		
+		return hash;
+	}
+}
